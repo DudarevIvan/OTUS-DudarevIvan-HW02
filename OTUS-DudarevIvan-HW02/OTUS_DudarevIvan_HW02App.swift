f6bbc47daa4fba84980a42a81da1184645e605f6
Dudarev Ivan <dudarevmail@icloud.com>
2021-03-26 09:21:01 +0500

    Initial Commit

---
+++ OTUS-DudarevIvan-HW02/OTUS_DudarevIvan_HW02App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_DudarevIvan_HW02App.swift
+//  OTUS-DudarevIvan-HW02
+//
+//  Created by Ivan Dudarev on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_DudarevIvan_HW02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
